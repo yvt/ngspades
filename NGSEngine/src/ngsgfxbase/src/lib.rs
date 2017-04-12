4ff8b9f5c4daa52c29212c2fd63d16ff64ee80e1
yvt <i@yvt.jp>
2017-04-12 10:00:18 +0900

    NGSEngine: Add the ngsgfxbase crate

---
+++ NGSEngine/src/ngsgfxbase/src/lib.rs
@@ -0,0 +1,13 @@
+//! # Nightingales Graphics Backend
+//!
+//! Abstracts the interface to the graphics APIs (e.g., Vulkan, Metal).
+//! Mostly based on [gfx-rs](https://github.com/gfx-rs/gfx).
+
+// TODO
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
