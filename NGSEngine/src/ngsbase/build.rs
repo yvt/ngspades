62dd556d6e6df4a07c10fe9707cf15eaff2f9ca0
yvt <i@yvt.jp>
2017-04-20 19:38:26 +0900

    ngsbase: Replace GenerateInterop.ps1 with build.rs

---
+++ NGSEngine/src/ngsbase/build.rs
@@ -0,0 +1,44 @@
+extern crate walkdir;
+
+use std::env;
+use std::process::Command;
+use std::path::Path;
+
+use walkdir::WalkDir;
+
+fn main() {
+    let ngsbase_path_str = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let ngsbase_path = Path::new(&ngsbase_path_str);
+    let ngsengine_path = ngsbase_path.parent().unwrap().parent().unwrap();
+    let project_path = ngsengine_path.parent().unwrap();
+
+    // Interop code generation tools
+    let interopgen_path = project_path.join("NGSRustInteropGen");
+    if !interopgen_path.exists() {
+        panic!("NGSRustInteropGen was not found.");
+    }
+    let interopgen_csproj_path = interopgen_path.join("NGSRustInteropGen.csproj");
+
+    // Interface definitions
+    let interop_path = project_path.join("NGSEngineInterop");
+
+    // Output file
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("interop.rs");
+
+    Command::new("dotnet")
+        .args(&["run",
+            "-p", interopgen_csproj_path.to_str().unwrap(),
+            "--",
+            "-o", dest_path.to_str().unwrap()])
+        .status()
+        .unwrap();
+
+    // emit cargo:rerun-if-changed
+    for entry in WalkDir::new("src") {
+        println!("cargo:rerun-if-changed={}", entry.unwrap().path().display());
+    }
+    for entry in WalkDir::new(interop_path) {
+        println!("cargo:rerun-if-changed={}", entry.unwrap().path().display());
+    }
+}
