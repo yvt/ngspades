6763ceb4c8b9496f575271c180d47a2209ba238c
yvt <i@yvt.jp>
2017-07-09 00:38:05 +0900

    NgsGFX: Update documentations

--- NGSEngine/src/ngsgfx/src/backend/vulkan/src/extif.rs
+++ NGSEngine/src/ngsgfx/src/backend/vulkan/src/extif.rs
@@ -23,8 +23,8 @@ pub type AshDevice = Device<V1_0>;
 ///    The device must not be destroyed until all references are removed.
 ///
 /// The backend provides [`OwnedDeviceRef`], which is a safe implementation of
-/// `DeviceRef`. This is implemented using `std::sync::Arc` and have a considerable
-/// amount of overhead for atomic operations. You can implement one by your own
+/// `DeviceRef`. This is implemented by using `std::sync::Arc` and might have
+/// some overhead for atomic operations. You can implement one by your own
 /// if you need a extra performance.
 ///
 /// [`OwnedDeviceRef`]: struct.OwnedDeviceRef.html
@@ -36,6 +36,8 @@ pub unsafe trait DeviceRef: Clone + Send + Sync + fmt::Debug + 'static {
     /// Since this trait requires `Sync` and this function's return type is
     /// a reference to `AllocationCallbacks`, the allocation functions are
     /// required to be thread-safe. (I wish Rust had higher-kinded types)
+    ///
+    /// Returns `None` by default.
     fn allocation_callbacks(&self) -> Option<&vk::AllocationCallbacks> { None }
 }
 
@@ -71,18 +73,30 @@ unsafe impl DeviceRef for OwnedDeviceRef {
 }
 
 impl OwnedDeviceRef {
-    /// Constructs an `OwnedDeviceRef` with a given device object.
+    /// Construct an `OwnedDeviceRef` with a given device object.
     ///
-    /// The ownership of `device` is transfered into the created `OwnedDeviceRef`.
+    /// The ownership of `device` will be transfered into the created `OwnedDeviceRef`.
     /// If you have any `ash::Device` that points the same instance of the device
     /// (for example, `ash::Device::clone` creates a new reference to the same instance),
     /// you must not destroy it after calling this function (this is one of the reasons why
     /// this function is marked as unsafe).
+    ///
+    /// `ash::Device` owned by `OwnedDeviceRef` will not be destroyed until all
+    /// instances of `OwnedDeviceRef` are dropped. The Vulkan specification states
+    /// that all child objects must be destroyed before their parent object is
+    /// destroyed, which means you cannot destroy the originating `ash::Instance`
+    /// safely until it is confirmed that all `ash::Device`s created from that are
+    /// destroyed. One way to ensure this is calling `OwnedDeviceRef::try_take`,
+    /// which will cause it to relinquish the ownership of the contained `ash::Device`
+    /// if there are no remaining references to it. Only if this function returns `Ok(x)`,
+    /// you can destroy `x` as well as the originating `ash::Instance` safely (supposing
+    /// all other objects created on it have been already destroyed).
     pub unsafe fn from_raw(device: AshDevice) -> Self {
         Self { device: Arc::new(UniqueDevice(device)) }
     }
 
-    /// Return the contained `ash::Device` if there is exactly one reference to it.
+    /// Return the contained `ash::Device` if there is exactly one reference to it
+    /// i.e. there exists no other `OwnedDeviceRef` pointing at the same `ash::Device`.
     pub fn try_take(self) -> Result<AshDevice, Self> {
         match Arc::try_unwrap(self.device) {
             Ok(dev) => {
