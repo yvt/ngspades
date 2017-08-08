5cc9ad1a48d75eaf2f5cce55bd79bc163f17ece5
yvt <i@yvt.jp>
2017-08-09 01:53:41 +0900

    YSR2: Create crates

---
+++ NGSEngine/src/ysr2/src/lib.rs
@@ -0,0 +1,9 @@
+//
+// Copyright 2017 yvt, all rights reserved.
+//
+// This source code is a part of Nightingales.
+//
+//! YSR2 - *Yonagi* Sound Renderer 2
+//! ================================
+//!
+pub extern crate ysr2_mixer as mixer;
