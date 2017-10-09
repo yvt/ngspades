0e401126ac69e033bcb97018992b601ed460b5a4
yvt <i@yvt.jp>
2017-10-10 03:08:07 +0900

    NgsPF: Create a crate for NgsPF

---
+++ NGSEngine/src/ngspf/src/lib.rs
@@ -0,0 +1,9 @@
+//
+// Copyright 2017 yvt, all rights reserved.
+//
+// This source code is a part of Nightingales.
+//
+//! Nightingales Presentation Framework (NgsPF)
+//! ===========================================
+//!
+//! todo
