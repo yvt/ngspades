abd445c3704eda1cd19846823b329e17577d3a28
yvt <i@yvt.jp>
2017-10-28 20:26:54 +0900

    Squashed 'NGSEngine/src/support/xalloc-rs/' content from commit ea96020
    git-subtree-dir: NGSEngine/src/support/xalloc-rs
    git-subtree-split: ea9602034901d650fb929028ccd30c82d0512389

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+//
+// Copyright 2017 yvt, all rights reserved.
+//
+// Licensed under the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>. This file may
+// not be copied, modified,or distributed except
+// according to those terms.
+//
+//! Dynamic suballocators for external memory (e.g., Vulkan device memory).
+//!
+//! # Provided Algorithms
+//!
+//! |               Name              | Time Complexity | Space Complexity |
+//! | ------------------------------- | --------------- | ---------------- |
+//! | TLSF (Two-Level Segregated Fit) | `O(1)`          | `O(N)`           |
+//! | Free space bitmap               | `O(size)`       | `O(size)`        |
+//!
+//! (`size`: heap size measured by the number of allocation units, `N`: number of allocations)
+//!
+//! # Examples
+//!
+//! ```
+//! use xalloc::{SysTlsf, SysTlsfRegion};
+//! let mut tlsf = xalloc::SysTlsf::new(8u32);
+//!
+//! // Allocate regions
+//! let alloc1: (SysTlsfRegion, u32) = tlsf.alloc(4).unwrap();
+//! let alloc2: (SysTlsfRegion, u32) = tlsf.alloc(4).unwrap();
+//! let (region1, offset1) = alloc1;
+//! let (region2, offset2) = alloc2;
+//! println!("allocated #1: {:?}", (&region1, offset1));
+//! println!("allocated #2: {:?}", (&region2, offset2));
+//!
+//! // Deallocate a region
+//! tlsf.dealloc(region1).unwrap();
+//!
+//! // Now we can allocate again
+//! tlsf.alloc(2).unwrap();
+//! tlsf.alloc(2).unwrap();
+//! ```
+//!
+//! # Feature Flags
+//!
+//! - `nightly` — Enables optimizations which currently require a Nightly Rust
+//!   compiler.
+//!
+#![cfg_attr(feature = "nightly", feature(nonzero))]
+pub extern crate num_traits;
+pub extern crate num_integer;
+
+pub mod arena;
+pub mod bitmap;
+mod bitmaputils;
+pub mod tlsf;
+pub mod int;
+
+pub use self::tlsf::{Tlsf, SafeTlsf, SysTlsf, TlsfBlock, TlsfRegion, SysTlsfRegion, SafeTlsfRegion};
+pub use self::bitmap::{BitmapAlloc, BitmapAllocRegion};
