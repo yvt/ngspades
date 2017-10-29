07ffbb447ccc5343c4905b3d25e8ac80cfd60cdf
yvt <i@yvt.jp>
2017-10-29 20:50:30 +0900

    NgsCOM: Update UnownedComPtr
    - Use ManuallyDrop instead of nullifying the contained ComPtr in the
      Drop implementation
    - Add the AsRef implementation

--- NGSEngine/src/ngscom/src/unownedcomptr.rs
+++ NGSEngine/src/ngscom/src/unownedcomptr.rs
@@ -5,8 +5,8 @@
 //
 
 use std::marker::PhantomData;
-use std::ops::{Deref, Drop};
-use std::ptr::null_mut;
+use std::ops::Deref;
+use std::mem::ManuallyDrop;
 
 use super::ComPtr;
 use super::comptr::{ComInterface, AsComPtr};
@@ -14,14 +14,14 @@ use super::comptr::{ComInterface, AsComPtr};
 /// Implicitly owned immutable COM pointer.
 #[derive(Debug)]
 pub struct UnownedComPtr<'a, T: ComInterface + 'a> {
-    comptr: ComPtr<T>,
+    comptr: ManuallyDrop<ComPtr<T>>,
     phantom: PhantomData<&'a T>,
 }
 
 impl<'a, T: ComInterface + 'a> UnownedComPtr<'a, T> {
     pub fn null() -> Self {
         UnownedComPtr {
-            comptr: ComPtr::null(),
+            comptr: ManuallyDrop::new(ComPtr::null()),
             phantom: PhantomData,
         }
     }
@@ -33,7 +33,7 @@ impl<'a, T: ComInterface + 'a> UnownedComPtr<'a, T> {
         let mut cp = ComPtr::<T>::new();
         *cp.as_mut_ptr() = x as *const T as *mut T;
         UnownedComPtr {
-            comptr: cp,
+            comptr: ManuallyDrop::new(cp),
             phantom: PhantomData,
         }
     }
@@ -45,7 +45,7 @@ impl<'a, T: ComInterface + 'a> UnownedComPtr<'a, T> {
         let mut cp = ComPtr::<T>::new();
         *cp.as_mut_ptr() = x.as_ptr::<T>();
         UnownedComPtr {
-            comptr: cp,
+            comptr: ManuallyDrop::new(cp),
             phantom: PhantomData,
         }
     }
@@ -55,13 +55,13 @@ impl<'a, T: ComInterface + 'a> Deref for UnownedComPtr<'a, T> {
     type Target = ComPtr<T>;
 
     fn deref(&self) -> &Self::Target {
-        &self.comptr
+        &*self.comptr
     }
 }
 
-impl<'a, T: ComInterface + 'a> Drop for UnownedComPtr<'a, T> {
-    fn drop(&mut self) {
-        // prevent releasing the object
-        *self.comptr.as_mut_ptr() = null_mut();
+impl<'a, T: ComInterface + 'a> AsRef<ComPtr<T>> for UnownedComPtr<'a, T> {
+    fn as_ref(&self) -> &ComPtr<T> {
+        &*self.comptr
     }
 }
+
