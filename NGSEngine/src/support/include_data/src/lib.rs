daf9f90cedde1ac37bf4b77088abbf419f2fcc9e
yvt <i@yvt.jp>
2018-02-26 22:02:28 +0900

    include_data: Fix to work on the latest Rust compiler

--- NGSEngine/src/support/include_data/src/lib.rs
+++ NGSEngine/src/support/include_data/src/lib.rs
@@ -13,7 +13,10 @@ use std::mem::size_of;
 /// endianness.
 #[macro_export]
 macro_rules! include_data {
-    ($file:expr) => ($crate::DataView(&$crate::AlignmentEnforcer(0, *include_bytes!($file)).1))
+    ($file:expr) => {{
+        const ENF: &$crate::AlignmentEnforcer<[u8]> = &$crate::AlignmentEnforcer(0, *include_bytes!($file));
+        $crate::DataView(&ENF.1)
+    }}
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -22,36 +25,34 @@ pub struct DataView(pub &'static [u8]);
 // This should make the storage at least 4-byte aligned... hahaha
 #[doc(hidden)]
 #[repr(C)]
-pub struct AlignmentEnforcer<T>(pub u32, pub T);
+pub struct AlignmentEnforcer<T: ?Sized>(pub u32, pub T);
 
 impl DataView {
-    pub unsafe fn unsafe_as_slice<T>(&self) -> &'static[T] {
+    pub unsafe fn unsafe_as_slice<T>(&self) -> &'static [T] {
         from_raw_parts(self.0.as_ptr() as *const T, self.0.len() / size_of::<T>())
     }
 
-    pub fn as_u8_slice(&self) -> &'static[u8] {
+    pub fn as_u8_slice(&self) -> &'static [u8] {
         self.0
     }
 
-    pub fn as_u16_slice(&self) -> &'static[u16] {
+    pub fn as_u16_slice(&self) -> &'static [u16] {
         unsafe { self.unsafe_as_slice::<u16>() }
     }
 
-    pub fn as_u32_slice(&self) -> &'static[u32] {
+    pub fn as_u32_slice(&self) -> &'static [u32] {
         unsafe { self.unsafe_as_slice::<u32>() }
     }
 
-    pub fn as_i8_slice(&self) -> &'static[i8] {
+    pub fn as_i8_slice(&self) -> &'static [i8] {
         unsafe { self.unsafe_as_slice::<i8>() }
     }
 
-    pub fn as_i16_slice(&self) -> &'static[i16] {
+    pub fn as_i16_slice(&self) -> &'static [i16] {
         unsafe { self.unsafe_as_slice::<i16>() }
     }
 
-    pub fn as_i32_slice(&self) -> &'static[i32] {
+    pub fn as_i32_slice(&self) -> &'static [i32] {
         unsafe { self.unsafe_as_slice::<i32>() }
     }
 }
-
-
