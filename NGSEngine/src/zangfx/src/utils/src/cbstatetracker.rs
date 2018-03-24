9e058e1518224bfebf8095a2f08d6d845ea3ce0f
yvt <i@yvt.jp>
2018-03-24 12:10:50 +0900

    ZanGFX Utils: CbStateTracker and Uploader are now Sync

--- NGSEngine/src/zangfx/src/utils/src/cbstatetracker.rs
+++ NGSEngine/src/zangfx/src/utils/src/cbstatetracker.rs
@@ -4,39 +4,61 @@
 // This source code is a part of Nightingales.
 //
 //! Tracks the execution state of command buffers.
-use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::{Condvar, Mutex};
 use std::time::Duration;
 use base;
 
 /// Tracks the execution state of a command buffer.
 #[derive(Debug)]
 pub struct CbStateTracker {
-    recv: mpsc::Receiver<()>,
+    state: Arc<State>,
+}
+
+#[derive(Debug)]
+struct State {
+    done: Mutex<bool>,
+    cv: Condvar,
 }
 
 impl CbStateTracker {
     pub fn new(cmd_buffer: &mut base::CmdBuffer) -> Self {
-        let (send, recv) = mpsc::sync_channel(0);
-        cmd_buffer.on_complete(Box::new(move || {
-            let _ = send;
-        }));
-        Self { recv }
+        let state = Arc::new(State {
+            done: Mutex::new(false),
+            cv: Condvar::new(),
+        });
+        {
+            let state = Arc::clone(&state);
+            cmd_buffer.on_complete(Box::new(move || {
+                let mut done = state.done.lock().unwrap();
+                *done = true;
+                state.cv.notify_all();
+            }));
+        }
+        Self { state }
     }
 
     pub fn is_completed(&self) -> bool {
-        self.recv.try_recv().is_err()
+        *self.state.done.lock().unwrap()
     }
 
     pub fn wait(&self) {
-        // This will unblock once `mpsc::Sender` is dropped
-        let _ = self.recv.recv();
+        let mut done = self.state.done.lock().unwrap();
+        while !*done {
+            done = self.state.cv.wait(done).unwrap();
+        }
     }
 
     pub fn wait_timeout(&self, timeout: Duration) -> Result<(), ()> {
-        match self.recv.recv_timeout(timeout) {
-            Ok(()) => unreachable!(),
-            Err(mpsc::RecvTimeoutError::Disconnected) => Ok(()),
-            Err(mpsc::RecvTimeoutError::Timeout) => Err(()),
+        let mut done = self.state.done.lock().unwrap();
+        while !*done {
+            // FIXME: This might block longer than intended in some cases
+            let (new_guard, result) = self.state.cv.wait_timeout(done, timeout).unwrap();
+            done = new_guard;
+            if result.timed_out() {
+                return Err(());
+            }
         }
+        Ok(())
     }
 }
