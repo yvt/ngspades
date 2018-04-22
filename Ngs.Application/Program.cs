e28f61fbb6766798c29b9853d673d16e2ea50df6
yvt <i@yvt.jp>
2018-04-22 19:00:06 +0900

    Ngs.Application: Update the test program to display some window

--- Ngs.Application/Program.cs
+++ Ngs.Application/Program.cs
@@ -4,14 +4,33 @@
 // This source code is a part of Nightingales.
 //
 using System;
-using System.Runtime.InteropServices;
 using Ngs.Engine;
+using Ngs.Utils;
 
 namespace Ngs.Shell {
     class MainClass {
         public static void Main(string[] args) {
-            Console.WriteLine("Checking if the engine core was successfully loaded");
-            EngineInstance.NativeEngine.CreateWorkspace();
+            Console.WriteLine("Displaying some window");
+            var engine = EngineInstance.NativeEngine;
+            var ws = engine.CreateWorkspace();
+
+            // Create and display a window
+            var window = ws.Context.CreateWindow();
+
+            var layer = ws.Context.CreateLayer();
+            layer.Bounds = new Box2(20, 20, 20, 20);
+            layer.SolidColor = Rgba.White;
+            window.Child = layer;
+
+            ws.Windows = window;
+            ws.Context.CommitFrame();
+
+            new System.Threading.Thread(() => {
+                System.Threading.Thread.Sleep(1000);
+                ws.Exit();
+                ws.Context.CommitFrame();
+            }).Start();
+            ws.Start();
         }
     }
 }
