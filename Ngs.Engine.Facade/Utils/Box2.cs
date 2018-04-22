6deca5f60e12e32db09528a59ab30d0076013473
yvt <i@yvt.jp>
2018-04-22 18:58:49 +0900

    Update Ngs.Utils.{Box2, Rgba} with more members

--- Ngs.Engine.Facade/Utils/Box2.cs
+++ Ngs.Engine.Facade/Utils/Box2.cs
@@ -13,7 +13,7 @@ namespace Ngs.Utils {
     /// <remarks>
     /// An AABB is represented by the minimum and maximum (corner) coordinates.
     /// </remarks>
-    [StructLayout (LayoutKind.Sequential)]
+    [StructLayout(LayoutKind.Sequential)]
     public struct Box2 {
         /// <summary>
         /// The minimum coordinate.
@@ -30,9 +30,95 @@ namespace Ngs.Utils {
         /// </summary>
         /// <param name="min">The minimum coordinate.</param>
         /// <param name="max">The maximum coordinate.</param>
-        public Box2 (Vector2 min, Vector2 max) {
+        public Box2(Vector2 min, Vector2 max) {
             Min = min;
             Max = max;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Box2" /> with given top-left coordinates
+        /// and extents.
+        /// </summary>
+        /// <param name="left">The X coordinate of the left edge of the box.
+        /// <c>Min.X</c> is initialized to this value.</param>
+        /// <param name="top">The X coordinate of the top edge of the box.
+        /// <c>Min.Y</c> is initialized to this value.</param>
+        /// <param name="width">The width (size in the X direction) of the box.
+        /// <c>Max.X</c> is calculated by adding <see paramref="left" /> to this value.</param>
+        /// <param name="height">The height (size in the Y direction) of the box.
+        /// <c>Max.Y</c> is calculated by adding <see paramref="top" /> to this value.</param>
+        public Box2(float left, float top, float width, float height) :
+            this(new Vector2(left, top), new Vector2(left + width, top + height)) {
+        }
+
+        /// <summary>
+        /// Retrieves the width (size in the X direction) of the box.
+        /// </summary>
+        /// <remarks>
+        /// The width of a box is calculated by <c>Max.X - Min.X</c>.
+        /// </remarks>
+        /// <returns>The width (size in the X direction) of the box.</returns>
+        public float Width { get => Max.X - Min.X; }
+
+        /// <summary>
+        /// Retrieves the height (size in the Y direction) of the box.
+        /// </summary>
+        /// <remarks>
+        /// The height of a box is calculated by <c>Max.Y - Min.Y</c>.
+        /// </remarks>
+        /// <returns>The height (size in the Y direction) of the box.</returns>
+        public float Height { get => Max.Y - Min.Y; }
+
+        /// <summary>
+        /// Sets (without moving the edge of the opposite side) or retrieves the
+        /// X coordinate of the left edge of the box.
+        /// </summary>
+        /// <remarks>
+        /// This property is equivalent to <c>Min.X</c>.
+        /// </remarks>
+        /// <returns>The X coordinate of the left edge of the box.</returns>
+        public float Left {
+            get => Min.X;
+            set => Min.X = value;
+        }
+
+        /// <summary>
+        /// Sets (without moving the edge of the opposite side) or retrieves the
+        /// X coordinate of the right edge of the box.
+        /// </summary>
+        /// <remarks>
+        /// This property is equivalent to <c>Max.X</c>.
+        /// </remarks>
+        /// <returns>The X coordinate of the ригчт edge of the box.</returns>
+        public float Right {
+            get => Max.X;
+            set => Max.X = value;
+        }
+
+        /// <summary>
+        /// Sets (without moving the edge of the opposite side) or retrieves the
+        /// Y coordinate of the top edge of the box.
+        /// </summary>
+        /// <remarks>
+        /// This property is equivalent to <c>Min.Y</c>.
+        /// </remarks>
+        /// <returns>The Y coordinate of the top edge of the box.</returns>
+        public float Top {
+            get => Min.Y;
+            set => Min.Y = value;
+        }
+
+        /// <summary>
+        /// Sets (without moving the edge of the opposite side) or retrieves the
+        /// Y coordinate of the bottom edge of the box.
+        /// </summary>
+        /// <remarks>
+        /// This property is equivalent to <c>Max.Y</c>.
+        /// </remarks>
+        /// <returns>The Y coordinate of the bottom edge of the box.</returns>
+        public float Bottom {
+            get => Max.Y;
+            set => Max.Y = value;
         }
     }
 }
--- Ngs.Engine.Facade/Utils/Rgba.cs
+++ Ngs.Engine.Facade/Utils/Rgba.cs
@@ -10,7 +10,7 @@ namespace Ngs.Utils {
     /// Represents a color value using four single precision floating point
     /// numbers.
     /// </summary>
-    [StructLayout (LayoutKind.Sequential)]
+    [StructLayout(LayoutKind.Sequential)]
     public struct Rgba {
         /// <summary>
         /// The red component.
@@ -39,11 +39,31 @@ namespace Ngs.Utils {
         /// <param name="green">The value to assign to the <see cref="Green" /> field.</param>
         /// <param name="blue">The value to assign to the <see cref="Blue" /> field.</param>
         /// <param name="alpha">The value to assign to the <see cref="Alpha" /> field.</param>
-        public Rgba (float red, float green, float blue, float alpha) {
+        public Rgba(float red, float green, float blue, float alpha) {
             Red = red;
             Green = green;
             Blue = blue;
             Alpha = alpha;
         }
+
+        /// <summary>
+        /// Retrieves a <see cref="Rgba" /> representing an opaque black color.
+        /// </summary>
+        public static readonly Rgba Black = new Rgba(0, 0, 0, 1);
+
+        /// <summary>
+        /// Retrieves a <see cref="Rgba" /> representing an opaque white color.
+        /// </summary>
+        public static readonly Rgba White = new Rgba(1, 1, 1, 1);
+
+        /// <summary>
+        /// Retrieves a <see cref="Rgba" /> representing a transparent black color.
+        /// </summary>
+        public static readonly Rgba TransparentBlack = new Rgba(0, 0, 0, 0);
+
+        /// <summary>
+        /// Retrieves a <see cref="Rgba" /> representing a transparent white color.
+        /// </summary>
+        public static readonly Rgba TransparentWhite = new Rgba(1, 1, 1, 0);
     }
 }
