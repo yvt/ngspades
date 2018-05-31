db75078d24bcd8918247feaaf13fc976c215bbef
yvt <i@yvt.jp>
2018-05-31 16:06:47 +0900

    Ngs.UI: Update `Ngs.UI.Alignment` to reduce the number of invalid states

--- Ngs.Framework/UI/Alignment.cs
+++ Ngs.Framework/UI/Alignment.cs
@@ -16,24 +16,24 @@ namespace Ngs.UI {
         /// Specifies to align an element with the right edge.
         Right = 0x2,
         /// Specifies to center an element horizontically.
-        HorizontalCenter = 0x4,
+        HorizontalCenter = 0,
         /// Specifies to justify an element horizontically.
-        HorizontalJustify = 0x8,
+        HorizontalJustify = Left | Right,
 
         /// The mask for the horizontal alignemnt flags.
-        HorizontalMask = 0xf,
+        HorizontalMask = 0x3,
 
         /// Specifies to align an element with the top edge.
         Top = 0x10,
         /// Specifies to align an element with the bottom edge.
         Bottom = 0x20,
         /// Specifies to center an element vertically.
-        VerticalCenter = 0x40,
+        VerticalCenter = 0,
         /// Specifies to justify an element vertically.
-        VerticalJustify = 0x40,
+        VerticalJustify = Top | Bottom,
 
         /// The mask for the vertical alignemnt flags.
-        VerticalMask = 0x70,
+        VerticalMask = 0x30,
 
         /// Specifies to center an element.
         Center = HorizontalCenter | VerticalCenter,
