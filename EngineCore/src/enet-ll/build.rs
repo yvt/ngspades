cefe13029481e02d9aa20ff3d51d67b1862d6a53
yvt <i@yvt.jp>
2018-07-11 01:50:30 +0900

    enet-ll: Fix to build on Linux

--- EngineCore/src/enet-ll/build.rs
+++ EngineCore/src/enet-ll/build.rs
@@ -1,7 +1,14 @@
 extern crate cc;
 
+use std::env;
+
 fn main() {
-    cc::Build::new()
+    let target = env::var("TARGET").unwrap();
+    let target_parts: Vec<_> = target.split('-').collect();
+
+    let mut build = cc::Build::new();
+
+    build
         .file("libenet/callbacks.c")
         .file("libenet/compress.c")
         .file("libenet/host.c")
@@ -11,6 +18,11 @@ fn main() {
         .file("libenet/protocol.c")
         .file("libenet/unix.c")
         .file("libenet/win32.c")
-        .include("libenet/include")
-        .compile("libenet.a");
+        .include("libenet/include");
+
+    if target_parts[2] == "linux" {
+        build.define("HAS_SOCKLEN_T", "1");
+    }
+
+    build.compile("libenet.a");
 }
