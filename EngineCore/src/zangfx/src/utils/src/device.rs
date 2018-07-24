0bc2c81f89d837adc07f4089f9d79b17adba97b5
yvt <i@yvt.jp>
2018-07-24 19:01:24 +0900

    ZanGFX Utils: `DeviceUtils` now works on concrete device types too

--- EngineCore/src/zangfx/src/utils/src/device.rs
+++ EngineCore/src/zangfx/src/utils/src/device.rs
@@ -9,7 +9,7 @@ use zangfx_base::{self as base, Result};
 use zangfx_common::BinaryInteger;
 
 /// An extension trait for `Device`.
-pub trait DeviceUtils {
+pub trait DeviceUtils: base::Device {
     /// Find the optimal memory type specific to the device based on the
     /// requirements.
     ///
@@ -19,53 +19,6 @@ pub trait DeviceUtils {
     ///   preferred by the application.
     /// - `required_caps` represents the capabilities the memory type must have.
     ///
-    fn choose_memory_type(
-        &self,
-        valid_memory_types: u32,
-        optimal_caps: base::MemoryTypeCapsFlags,
-        required_caps: base::MemoryTypeCapsFlags,
-    ) -> Option<base::MemoryType>;
-
-    /// Get the supported memory types for the given buffer usage.
-    fn memory_types_for_buffer(&self, usage: base::BufferUsageFlags) -> Result<u32>;
-
-    /// Get the supported memory types for the given image format.
-    ///
-    /// The returned values are guaranteed to be identical for all color
-    /// formats. This, however, does not apply to depth/stencil formats.
-    /// The backend implementation should ensure that as per Vulkan 1.0
-    /// "11.6. Resource Memory Association".
-    fn memory_types_for_image(&self, format: base::ImageFormat) -> Result<u32>;
-
-    /// Find the optimal memory type for the given buffer usage.
-    ///
-    /// Unless any error occurs during the process, this method is guaranteed to
-    /// return some memory type provided that `required_caps` is a subset of at
-    /// least one of the following: `DeviceLocal` and
-    /// `HostVisible | HostCoherent`.
-    fn memory_type_for_buffer(
-        &self,
-        usage: base::BufferUsageFlags,
-        optimal_caps: base::MemoryTypeCapsFlags,
-        required_caps: base::MemoryTypeCapsFlags,
-    ) -> Result<Option<base::MemoryType>>;
-
-    /// Find the optimal memory type for the given image format.
-    ///
-    /// Unless any error occurs during the process, this method is guaranteed to
-    /// return some memory type provided that `required_caps` is a subset of at
-    /// least one of the following: `DeviceLocal`.
-    ///
-    /// Note: Images are never host-visible in ZanGFX.
-    fn memory_type_for_image(
-        &self,
-        format: base::ImageFormat,
-        optimal_caps: base::MemoryTypeCapsFlags,
-        required_caps: base::MemoryTypeCapsFlags,
-    ) -> Result<Option<base::MemoryType>>;
-}
-
-impl DeviceUtils for dyn base::Device {
     fn choose_memory_type(
         &self,
         valid_memory_types: u32,
@@ -91,11 +44,18 @@ impl DeviceUtils for dyn base::Device {
         None
     }
 
+    /// Get the supported memory types for the given buffer usage.
     fn memory_types_for_buffer(&self, usage: base::BufferUsageFlags) -> Result<u32> {
         let buffer = self.build_buffer().size(1).usage(usage).build()?;
         Ok(buffer.get_memory_req()?.memory_types)
     }
 
+    /// Get the supported memory types for the given image format.
+    ///
+    /// The returned values are guaranteed to be identical for all color
+    /// formats. This, however, does not apply to depth/stencil formats.
+    /// The backend implementation should ensure that as per Vulkan 1.0
+    /// "11.6. Resource Memory Association".
     fn memory_types_for_image(&self, format: base::ImageFormat) -> Result<u32> {
         let image = self
             .build_image()
@@ -106,6 +66,12 @@ impl DeviceUtils for dyn base::Device {
         Ok(image.get_memory_req()?.memory_types)
     }
 
+    /// Find the optimal memory type for the given buffer usage.
+    ///
+    /// Unless any error occurs during the process, this method is guaranteed to
+    /// return some memory type provided that `required_caps` is a subset of at
+    /// least one of the following: `DeviceLocal` and
+    /// `HostVisible | HostCoherent`.
     fn memory_type_for_buffer(
         &self,
         usage: base::BufferUsageFlags,
@@ -116,6 +82,13 @@ impl DeviceUtils for dyn base::Device {
         Ok(self.choose_memory_type(types, optimal_caps, required_caps))
     }
 
+    /// Find the optimal memory type for the given image format.
+    ///
+    /// Unless any error occurs during the process, this method is guaranteed to
+    /// return some memory type provided that `required_caps` is a subset of at
+    /// least one of the following: `DeviceLocal`.
+    ///
+    /// Note: Images are never host-visible in ZanGFX.
     fn memory_type_for_image(
         &self,
         format: base::ImageFormat,
@@ -126,3 +99,5 @@ impl DeviceUtils for dyn base::Device {
         Ok(self.choose_memory_type(types, optimal_caps, required_caps))
     }
 }
+
+impl<T: base::Device + ?Sized> DeviceUtils for T {}
