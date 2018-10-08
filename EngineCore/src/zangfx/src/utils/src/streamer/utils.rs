685d3ae4ce5ea5fe00d32b6f791f4e514a12082b
yvt <i@yvt.jp>
2018-10-08 21:11:10 +0900

    ZanGFX Utils: Add `Stage`, a union type of `StageBuffer` and `StageImage`

--- EngineCore/src/zangfx/src/utils/src/streamer/utils.rs
+++ EngineCore/src/zangfx/src/utils/src/streamer/utils.rs
@@ -65,3 +65,56 @@ impl<'a> StreamerRequest for StageImage<'a> {
         Ok(())
     }
 }
+
+/// A tagged union of [`StageBuffer`] and [`StageImage`], implementing
+/// [`StreamerRequest`].
+#[derive(Debug, Clone)]
+pub enum Stage<'a> {
+    Buffer(StageBuffer<'a>),
+    Image(StageImage<'a>),
+}
+
+impl<'a> From<StageBuffer<'a>> for Stage<'a> {
+    fn from(x: StageBuffer<'a>) -> Self {
+        Stage::Buffer(x)
+    }
+}
+
+impl<'a> From<StageImage<'a>> for Stage<'a> {
+    fn from(x: StageImage<'a>) -> Self {
+        Stage::Image(x)
+    }
+}
+
+impl<'a> StreamerRequest for Stage<'a> {
+    fn size(&self) -> usize {
+        match self {
+            Stage::Buffer(inner) => StreamerRequest::size(inner),
+            Stage::Image(inner) => StreamerRequest::size(inner),
+        }
+    }
+
+    fn populate(&mut self, staging_buffer: &mut [u8]) {
+        match self {
+            Stage::Buffer(inner) => StreamerRequest::populate(inner, staging_buffer),
+            Stage::Image(inner) => StreamerRequest::populate(inner, staging_buffer),
+        }
+    }
+
+    fn copy(
+        &mut self,
+        encoder: &mut dyn base::CopyCmdEncoder,
+        staging_buffer: &base::BufferRef,
+        staging_buffer_range: Range<base::DeviceSize>,
+        phase: u32,
+    ) -> Result<()> {
+        match self {
+            Stage::Buffer(inner) => {
+                StreamerRequest::copy(inner, encoder, staging_buffer, staging_buffer_range, phase)
+            }
+            Stage::Image(inner) => {
+                StreamerRequest::copy(inner, encoder, staging_buffer, staging_buffer_range, phase)
+            }
+        }
+    }
+}
