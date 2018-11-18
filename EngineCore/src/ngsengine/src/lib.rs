340fcfd5df9ac694f3e69f3513610771bbec55ea
yvt <i@yvt.jp>
2018-11-18 17:35:43 +0900

    Disable jemalloc on `*-windows-msvc`
    `jemallocator` does not build on `*-windows-msvc` (yet).

--- EngineCore/src/ngsengine/src/lib.rs
+++ EngineCore/src/ngsengine/src/lib.rs
@@ -7,6 +7,8 @@ mod entry;
 
 pub use self::entry::ngsengine_create;
 
-// Register jemalloc as the global allocator
+// Register jemalloc as the global allocator.
+// Disable this on `*-windows-msvc` since `jemallocator` doesn't build on it yet.
+#[cfg(any(not(target_os = "windows"), target_env = "gnu"))]
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
