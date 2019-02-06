39f3e9e521748bd03f7c0ab52b3f9d4f0a48b3db
yvt <i@yvt.jp>
2019-02-06 11:17:02 +0900

    Stygian: Add a benchmark test for rasterization

--- EngineCore/src/stygian/benches/terrainrast.rs
+++ EngineCore/src/stygian/benches/terrainrast.rs
@@ -3,10 +3,14 @@
 //
 // This source code is a part of Nightingales.
 //
-use bencher::{Bencher, benchmark_group, benchmark_main};
-use cgmath::{Matrix4, Perspective, Point3, vec3};
+use bencher::{benchmark_group, benchmark_main, Bencher};
+use cgmath::{vec2, vec3, Matrix4, Perspective, Point3};
 
-use stygian::TerrainRast;
+use stygian::{DepthImage, Terrain, TerrainRast};
+
+#[path = "../examples/lib/terrainload.rs"]
+#[allow(dead_code)]
+mod terrainload;
 
 fn set_camera_matrix(b: &mut Bencher) {
     let mut rast = TerrainRast::new(512);
@@ -28,5 +32,54 @@ fn set_camera_matrix(b: &mut Bencher) {
     });
 }
 
-benchmark_group!(benches, set_camera_matrix);
+fn rasterize(b: &mut Bencher, size: usize) {
+    let mut rast = TerrainRast::new(size);
+
+    let projection: Matrix4<f32> = Perspective {
+        left: -0.5,
+        right: 0.5,
+        top: 0.5,
+        bottom: -0.5,
+        near: 1.0,
+        far: 100.0,
+    }
+    .into();
+
+    let projection = Matrix4::from_translation(vec3(0.0, 0.0, 1.0))
+        * Matrix4::from_nonuniform_scale(1.0, 1.0, -1.0)
+        * projection;
+
+    let view = Matrix4::look_at(
+        Point3::new(64.0, 64.0, 15.0),
+        Point3::new(0.0, 0.0, 5.0),
+        vec3(0.0, 0.0, 1.0),
+    );
+    rast.set_camera_matrix(projection * view);
+
+    let mut image = DepthImage::new(vec2(size, size));
+
+    let sty_terrain = Terrain::from_ngsterrain(&terrainload::DERBY_RACERS).unwrap();
+
+    b.iter(|| {
+        rast.rasterize(&sty_terrain, &mut image);
+    });
+}
+
+fn rasterize_64(b: &mut Bencher) {
+    rasterize(b, 64);
+}
+fn rasterize_256(b: &mut Bencher) {
+    rasterize(b, 256);
+}
+fn rasterize_1024(b: &mut Bencher) {
+    rasterize(b, 1024);
+}
+
+benchmark_group!(
+    benches,
+    set_camera_matrix,
+    rasterize_64,
+    rasterize_256,
+    rasterize_1024
+);
 benchmark_main!(benches);
