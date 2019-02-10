2d4a2e21bfec6912e50720815b540e0747b7aba5
yvt <i@yvt.jp>
2019-02-10 20:54:25 +0900

    Add a crate `alt_fp`

---
+++ EngineCore/src/support/alt_fp/src/cmp.rs
@@ -0,0 +1,140 @@
+//
+// Copyright 2018 yvt, all rights reserved.
+//
+// This source code is a part of Nightingales.
+//
+//! Fast maximum/minimum value functions for floating-point types.
+
+/// Implements fast maximum/minimum value functions for floating-point types.
+///
+/// # Implementation notes
+///
+/// The method defintions of `FloatOrd` are modeled after x86's `minss`/`maxss`
+/// instructions so that they can be efficiently implemented on x86 processors.
+///
+/// # Examples
+///
+///     use alt_fp::FloatOrd;
+///
+///     assert_eq!(1.0.fmin(2.0), 1.0);
+///     assert_eq!(2.0.fmin(1.0), 1.0);
+///
+///     assert_eq!(1.0.fmax(2.0), 2.0);
+///     assert_eq!(2.0.fmax(1.0), 2.0);
+///
+/// They handle NaN differently from `<f32>::{min, max}`:
+///
+///     assert!(<f32>::from_bits(0x7f801234).is_nan());
+///     assert!(<f32>::from_bits(0x7f804321).is_nan());
+///
+///     assert_eq!(<f32>::from_bits(0x7f801234)
+///         .fmin(<f32>::from_bits(0x7f804321)).to_bits(), 0x7f804321);
+///     assert_eq!(<f32>::from_bits(0x7f801234)
+///         .fmax(<f32>::from_bits(0x7f804321)).to_bits(), 0x7f804321);
+///
+///     // Compare the above with:
+///     assert_ne!(<f32>::from_bits(0x7f801234)
+///         .min(<f32>::from_bits(0x7f804321)).to_bits(), 0x7f804321);
+///
+pub trait FloatOrd {
+    /// Compute the minimum value of `self` and `x`. Returns `x` if `self`
+    /// is NaN.
+    fn fmin(self, x: Self) -> Self
+    where
+        Self: Sized;
+
+    /// Compute the maximum value of `self` and `x`. Returns `x` if `self`
+    /// is NaN.
+    fn fmax(self, x: Self) -> Self
+    where
+        Self: Sized;
+}
+
+impl FloatOrd for f32 {
+    #[inline]
+    fn fmin(self, x: Self) -> Self {
+        if self < x {
+            self
+        } else {
+            x
+        }
+    }
+
+    #[inline]
+    fn fmax(self, x: Self) -> Self {
+        if self > x {
+            self
+        } else {
+            x
+        }
+    }
+}
+
+impl FloatOrd for f64 {
+    #[inline]
+    fn fmin(self, x: Self) -> Self {
+        if self < x {
+            self
+        } else {
+            x
+        }
+    }
+
+    #[inline]
+    fn fmax(self, x: Self) -> Self {
+        if self > x {
+            self
+        } else {
+            x
+        }
+    }
+}
+
+/// A set of `FloatOrd` values.
+pub trait FloatOrdSet {
+    type Item;
+
+    /// Compute the minimum value of the set. Panics if the set is empty.
+    #[inline]
+    fn fmin(&self) -> Self::Item;
+
+    /// Compute the maximum value of the set. Panics if the set is empty.
+    #[inline]
+    fn fmax(&self) -> Self::Item;
+}
+
+impl<T: FloatOrd + Copy> FloatOrdSet for [T] {
+    type Item = T;
+
+    #[inline]
+    fn fmin(&self) -> Self::Item {
+        let mut output = self[0];
+        for &x in &self[1..] {
+            output = output.fmin(x);
+        }
+        output
+    }
+
+    #[inline]
+    fn fmax(&self) -> Self::Item {
+        let mut output = self[0];
+        for &x in &self[1..] {
+            output = output.fmax(x);
+        }
+        output
+    }
+}
+
+impl<T: FloatOrd + Copy> FloatOrdSet for [T; 4] {
+    type Item = T;
+
+    #[inline]
+    fn fmin(&self) -> Self::Item {
+        [[self[0], self[1]].fmin(), [self[2], self[3]].fmin()].fmin()
+    }
+
+    #[inline]
+    fn fmax(&self) -> Self::Item {
+        [[self[0], self[1]].fmax(), [self[2], self[3]].fmax()].fmax()
+    }
+}
---
+++ EngineCore/src/support/alt_fp/src/lib.rs
@@ -0,0 +1,12 @@
+//
+// Copyright 2019 yvt, all rights reserved.
+//
+// This source code is a part of Nightingales.
+//
+//! This crate provides an alternative (faster in most cases) implementation for
+//! floating-point operations.
+pub mod cast;
+pub mod cmp;
+
+#[doc(no_inline)]
+pub use self::{cast::*, cmp::*};
